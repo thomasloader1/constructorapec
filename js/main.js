6fc7aa5c29880b3e8f1e8f733ebf7578d23a07c0
Gomez Tomas Gonzalo <tomas.gomez@aufieroinformatica.com.ar>
2020-06-15 13:38:27 -0300

    implementacion de fetch

--- js/main.js
+++ js/main.js
@@ -1,62 +1,38 @@
-const iconsContainer = document.querySelector("#iconContainer");
+const navMenu = document.querySelector('#navigation');
+const itemGridContent = $(`#item-grid-content`)
 
-console.log(iconsContainer.childNodes)
-
-const iconWsp = document.querySelector("#iconWsp");
-const iconFace = document.querySelector("#iconFace");
-const iconContact = document.querySelector("#iconContact");
-const inputContainerWsp = document.querySelector("#inputContainerWsp");
-
-//
-
-const homeContent = document.querySelector("#homeContent");
-const formContainer = document.querySelector("#formContainer");
-const homeBotton = document.querySelector("#homeButton");
-const iconPhone = document.querySelector("#iconPhone");
-
-iconContact.addEventListener('click', () => {
-    homeContent.classList.add('d-none', 'fadeOutLeft');
-    formContainer.classList.toggle('d-none');
+navMenu.addEventListener('click', e =>{
+    console.log(e.target.innerHTML);
+    let { target } = e;
+    target.nodeName === "A" ? setMiddleContent(`${target.innerHTML}-grid`, target) : console.log("No es un link")
 });
 
-homeBotton.addEventListener('click', () => {
-    formContainer.classList.toggle('d-none', 'fadeOutLeft');
-    homeContent.classList.remove('d-none');
-});
+const setMiddleContent = ( nameGridContainer , element) =>{
+    switch (nameGridContainer){
+        case 'home-grid':
+            $('.item-grid-img').delay("fast").fadeOut();
+            $('.nav-grid').delay("fast").fadeOut();
+            
+            setTimeout(() => {
+                $('.item-grid-img').css('height', '100%').delay("fast").fadeIn()
+                $(`#${nameGridContainer}`).delay("slow").css("grid-template-rows", "10% 75% 15%")
+                itemGridContent.toggleClass("d-none").css('overflow', 'scroll');
+                $('.nav-grid').css('align-self', 'center');
+                $('.nav-grid').delay("fast").fadeIn();
+            }, 700);
+        break;
+        default :
+            $('.item-grid-img').css('height', '%').delay("fast").fadeIn()
+            $(`#${nameGridContainer}`).delay("slow").css("grid-template-rows", "repeat(2, 50%)")
+        
+    }
 
-const formContact = document.getElementById('formContact');
+    
+}
 
-const phoneData = document.querySelector("#phoneData");
-// iconPhone.addEventListener('click', () => {
-//     iconPhone.classList.toggle('active-icon');
-//     phoneData.classList.toggle('d-none');
-// });
+const getPage = (namePage) =>{
+    $.ajax({ url: `${namePage}.html`, success: function(res){
+        itemGridContent.html(res)
+    }})
+}
 
-let templateSuccess = `<div class="alert alert-success" role="alert">
-<h4 class="alert-heading font-weight-bold">Muchas Gracias!</h4>
-<p>Tu mensaje se a enviado correctamente y a la brevedad nos pondremos en contacto contigo</p>
-<hr>
-<p class="mb-0 mt-3">Cualquier consulta que necesites realizar de forma inmediata podes enviarnos un correo electronico a <strong>info@solucionespec.com</strong> o enviarnos un <a href="https://wa.me/5491152491995?text=Hola%2C%20que%20tal%3F%2C%20vengo%20desde%20tu%20web%20y%20necesito%20que%20me%20asesores.." target="_blank" class="font-weight-bold text-success">Whatsapp</a></p>
-</div>`;
-
-$('#formContact').submit(function(e) {
-    e.preventDefault();
-    const postValue = {
-        name: $('#name').val(),
-        email: $('#emailInput').val(),
-        phone: $('#phone').val(),
-        message: $('#message').val(),
-    };
-    //console.log(postValue)
-    $.post('php/addMessage.php', postValue, function(res) {
-        console.log(res)
-        if (res === '200') {
-            $('#successMessage').html(templateSuccess)
-            $('#formContact').addClass('d-none')
-            $('#formContainer').addClass('d-none')
-            $('#successMessageContainer').removeClass('d-none').addClass('fadeInLeft');
-        }
-
-        $("formContact").trigger('reset');
-    });
-});
